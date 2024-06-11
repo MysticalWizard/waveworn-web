fb374e5716d21d5d33de9744bbdb56fc4c7f5cb1
Wonsik Shin <mmaring0120@gmail.com>
2024-06-12 03:31:22 +0800

    feat: add footer styles

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,18 @@
+import Link from 'next/link';
+
 export function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <footer>
-      <div>footer</div>
+    <footer className="flex border-t bg-background">
+      <div className="p-4">
+        <span className="text-xs sm:text-sm text-muted-foreground">
+          &copy; {currentYear}{' '}
+          <Link href="/" className="transition-all hover:text-primary">
+            Wuthering Waves Tools
+          </Link>
+        </span>
+      </div>
     </footer>
   );
 }
