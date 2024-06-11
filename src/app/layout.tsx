0913dad5d855673a25c9228f17a09b928118ad1a
Wonsik Shin <mmaring0120@gmail.com>
2024-06-12 03:02:29 +0800

    feat: add header and mode toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import './globals.css';
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
 import { PageHeader } from '@/components/page-header';
 import { PageFooter } from '@/components/page-footer';
+import { cn } from '@/lib/utils';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,6 +14,13 @@ export const metadata: Metadata = {
     default: 'Wuthering Waves Tools',
   },
   description: 'Various tools for Wuthering Waves',
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#ffffff' },
+    { media: '(prefers-color-scheme: dark)', color: '#000000' },
+  ],
 };
 
 export default function RootLayout({
@@ -29,9 +37,12 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <div id="root">
+          <div
+            id="root"
+            className="relative flex min-h-screen flex-col bg-background"
+          >
             <PageHeader />
-            <main>{children}</main>
+            <main className="flex-1">{children}</main>
             <PageFooter />
           </div>
         </ThemeProvider>
