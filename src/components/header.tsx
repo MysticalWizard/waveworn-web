b8487cd7b47883fd09a2f18cb888d6abb6dc6df3
Wonsik Shin <mmaring0120@gmail.com>
2024-06-13 00:24:09 +0800

    feat: add convene page to nav
    + add mobile nav

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,11 @@
-import Link from 'next/link';
 import { ModeToggle } from './mode-toggle';
+import { Nav } from './nav';
 
 export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 p-2 flex justify-center md:px-4">
       <div className="flex items-center w-full max-w-screen-desktop">
-        <div className="flex items-center flex-1 space-x-2">
-          <Link href="/">Wuthering Waves Tools</Link>
-        </div>
+        <Nav />
         <div className="flex items-center justify-end flex-1 space-x-2">
           <ModeToggle />
         </div>
